eb65744bfd41923804c76127d28176987ef978e2
Patrick O'Connor <patrick.oconnor@workiva.com>
2016-02-05 23:09:36 -0700

    Lower range so client ports are not on server port

--- Ports/ports.h
+++ Ports/ports.h
@@ -1,7 +1,7 @@
 #ifndef __PORTS_INCLUDED__
 #define __PORTS_INCLUDED__
 #include <cstdlib>
-const int MAX_PORT = 4923;
+const int MAX_PORT = 4922; //Technically, 4923 is valid but that is reserved for the server
 const int MIN_PORT = 4824;
 const int RANGE_PORT = MAX_PORT - MIN_PORT;
 
