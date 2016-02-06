e87184d4407342c2c1f8138fc3c4ba4750e0dfab
Patrick O'Connor <patrick.oconnor@workiva.com>
2016-02-05 23:56:39 -0700

    Add util for raw message len

---
+++ Utils/file_utils.h
@@ -0,0 +1,6 @@
+#ifndef __FILE_UTILS_INCLUDED__
+#define __FILE_UTILS_INCLUDED__
+int get_raw_message_len(const char* message) {
+  // TODO: Takes the message. Builds a dummy packet. Returns len(serialize).
+}
+#endif
