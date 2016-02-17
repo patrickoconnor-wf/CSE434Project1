b81e3ace7fc20777bf7b8b295068382eaedd575a
Patrick O'Connor <patrick.oconnor@workiva.com>
2016-02-16 22:41:57 -0700

    Add working implementation of getting file size and listing

--- Utils/file_utils.cpp
+++ Utils/file_utils.cpp
@@ -1,8 +1,11 @@
 #include "file_utils.h"
 
 const char* getFileSystemContents() {
-  std::string share_dir = "./UDPClient/share";
-  DIR *open = opendir(share_dir.c_str());
+  // Get the Current Working Directory
+  char *cwdBuffer = new char[512];
+
+  sprintf(cwdBuffer, "%s/%s", getcwd(cwdBuffer, 1024), "UDPClient/share/");
+  DIR *open = opendir(cwdBuffer);
   struct stat filestat;
   if (open == NULL) {
     printf("%s%s\n", "ERROR: Directory structure is incorrect. The client should ",
@@ -10,38 +13,28 @@ const char* getFileSystemContents() {
     exit(1);
   }
 
-  std::string listings_str = "";
+  char *listings_str = new char[5120];
   struct dirent *dirp;
   while ((dirp = readdir(open)) != NULL) {
-       std::cout << dirp->d_name << std::endl;
-       char *d_name = dirp->d_name;
-       std::string filename = share_dir;
-       filename += "/";
-       filename += d_name;
-      //  if (stat(filename.c_str(), &filestat)) continue;
-      //  if (S_ISDIR(filestat.st_mode)) continue;
-       long f_size = getFileSize(d_name);
-       listings_str += d_name;
-       listings_str += "|";
-       listings_str += std::to_string(f_size);
-       listings_str += "\n";
+      std::string filename = cwdBuffer;
+      filename += dirp->d_name;
+      int f_size = getFileSize(filename.c_str());
+      sprintf(listings_str, "%s|%d\n", dirp->d_name, f_size);
    }
-   std::cout << listings_str << std::endl;
-   const char *buffer = listings_str.c_str();
-   return buffer;
+   printf("%s\n", listings_str);
+   return listings_str;
+
 }
 
-long getFileSize(const char *fileName) {
-    struct stat filestat;
-    // std::streampos begin,end;
-    // std::ifstream in(fileName, std::ios::binary);
-    // begin = in.tellg();
-    // in.seekg(0, in.end);
-    // end = in.tellg();
-    // std::cout << "Begin: " << begin << std::endl;
-    // std::cout << "End: " << end << std::endl;
-    // in.close();
-    // return end;
-    int rc = stat(fileName, &filestat);
-    return rc == 0 ? filestat.st_size : -1;
+int getFileSize(const char *fileName) {
+  std::ifstream file(fileName, std::ifstream::in | std::ifstream::binary);
+
+  if (!file.good()) {
+    printf("%s\n", "File is not open. The given file size will be incorrect.");
+  }
+
+  file.seekg(0, std::ios::end);
+  int fileSize = file.tellg();
+  file.close();
+  return fileSize;
 }
