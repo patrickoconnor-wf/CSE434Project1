6de9c70b9ab0fe9795e47c9606cd5cd0c87ba90d
Patrick O'Connor <patrick.oconnor@workiva.com>
2016-02-23 15:07:00 -0700

    Add implementations for static functions

--- ClientInfo/clientinfo.cpp
+++ ClientInfo/clientinfo.cpp
@@ -1,22 +1,23 @@
 #include "clientinfo.h"
-#include <iostream>
 
-ClientInfo::ClientInfo(char *hostName,char *ipAddress ) {
+// This makes sure the cpp and header refer to the same vector
+std::vector<ClientInfo> ClientInfo::clients;
+
+ClientInfo::ClientInfo(char *hostName, char *ipAddress ) {
   this->hostName = hostName;
   this->ipAddress = ipAddress;
 //  this->fileNames = new std::vector<std::string>;
 }
 
-ClientInfo::~ClientInfo() {
-  delete [] this->hostName;
-  delete [] this->ipAddress;
-}
+ClientInfo::~ClientInfo() {}
+
 char* ClientInfo::formatFilesList (char *message){
   std::vector<std::string> FileNames = split(message, ' ');
   this->fileNames = FileNames;
   static char ch[] = "ACK";
       return ch;
 }
+
 char* ClientInfo::getHostName() {
   return this->hostName;
 }
@@ -24,12 +25,34 @@ char* ClientInfo::getHostName() {
 char* ClientInfo::getIpAddress() {
   return this->ipAddress;
 }
+
+std::vector<ClientInfo> ClientInfo::getClients() {
+  return clients;
+}
+
 bool ClientInfo::fileFoundInClient(const char *FileBeingSearched){
-  bool Found = false ;
+  bool Found = false;
   std::string sthisFileName = std::string(FileBeingSearched);
     /* data */
   if(std::find(this->fileNames.begin(), this->fileNames.end(), sthisFileName) != this->fileNames.end()) {
-   Found = true ;
+   Found = true;
   }
   return Found;
+}
+
+std::string ClientInfo::getClientsByFileName(const char *fileName){
+  std::string message = "";
+  for(std::vector<ClientInfo>::iterator iter = clients.begin(); iter != clients.end(); iter++) {
+      if(iter->fileFoundInClient(fileName))
+      {
+        std::cout << iter->getHostName();
+        //message += message + iter->getHostName();
+      }
+  }
+      return message.c_str();
+}
+
+void ClientInfo::removeClient(char* HostName, char *IpAddress)
+{
+  //Clients.erase(std::remove(Clients.begin(),Clients.end(),)Clients.end());
 }
